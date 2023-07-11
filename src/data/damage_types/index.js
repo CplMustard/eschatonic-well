fb8276f1b3208b822d191d83027cfe216d9bb175
Jack Anderson <corporalmustard@gmail.com>
2023-07-11 09:43:10 -0700

    add more damage type names

--- src/data/damage_types/index.js
+++ src/data/damage_types/index.js
@@ -1,14 +1,24 @@
 import ballistic from "./ballistic.json";
 import beam from "./beam.json";
+import blast from "./blast.json";
+import cold from "./cold.json";
+import corrosion from "./corrosion.json";
 import energy from "./energy.json";
 import explosion from "./explosion.json";
+import fire from "./fire.json";
+import force from "./force.json";
 import kinetic from "./kinetic.json";
 
 const damage_typesData = {
 	ballistic: ballistic,
 	beam: beam,
+	blast: blast,
+	cold: cold,
+	corrosion: corrosion,
 	energy: energy,
 	explosion: explosion,
+	fire: fire,
+	force: force,
 	kinetic: kinetic
 }
 export default damage_typesData;
