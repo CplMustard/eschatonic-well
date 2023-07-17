db04c1df8c7415b35cc95275c2191e2f105e2ba1
Jack Anderson <corporalmustard@gmail.com>
2023-07-17 09:20:35 -0700

    add furies

--- src/data/cyphers/index.js
+++ src/data/cyphers/index.js
@@ -1,8 +1,32 @@
 import arcane_synthesis from "./arcane_synthesis.json";
+import atrophic_decomposer from "./atrophic_decomposer.json";
+import cacophonic_declination from "./cacophonic_declination.json";
+import cryo_lock from "./cryo_lock.json";
+import havoc_spiral from "./havoc_spiral.json";
+import instability_equation from "./instability_equation.json";
+import malediction_rubric from "./malediction_rubric.json";
+import nihilation_vortex from "./nihilation_vortex.json";
+import null_collider from "./null_collider.json";
+import paralytic_disruptor from "./paralytic_disruptor.json";
+import psychonomic_lash from "./psychonomic_lash.json";
 import pyrokinetic_surge from "./pyrokinetic_surge.json";
+import velocity_projector from "./velocity_projector.json";
+import volition_anchor from "./volition_anchor.json";
 
 const cyphersData = {
 	arcane_synthesis: arcane_synthesis,
-	pyrokinetic_surge: pyrokinetic_surge
+	atrophic_decomposer: atrophic_decomposer,
+	cacophonic_declination: cacophonic_declination,
+	cryo_lock: cryo_lock,
+	havoc_spiral: havoc_spiral,
+	instability_equation: instability_equation,
+	malediction_rubric: malediction_rubric,
+	nihilation_vortex: nihilation_vortex,
+	null_collider: null_collider,
+	paralytic_disruptor: paralytic_disruptor,
+	psychonomic_lash: psychonomic_lash,
+	pyrokinetic_surge: pyrokinetic_surge,
+	velocity_projector: velocity_projector,
+	volition_anchor: volition_anchor
 }
 export default cyphersData;
