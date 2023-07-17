1fc5dc5b58276f5ab4ba0005c946bbf3e154b2e7
Jack Anderson <corporalmustard@gmail.com>
2023-07-17 16:48:31 -0700

    add harmonics

--- src/data/cyphers/index.js
+++ src/data/cyphers/index.js
@@ -1,15 +1,26 @@
 import acheronian_venediction from "./acheronian_venediction.json";
+import aggression_theorem from "./aggression_theorem.json";
+import arcanesscent_regenerator from "./arcanesscent_regenerator.json";
 import arcane_synthesis from "./arcane_synthesis.json";
 import atrophic_decomposer from "./atrophic_decomposer.json";
 import cacophonic_declination from "./cacophonic_declination.json";
+import canker_bane from "./canker_bane.json";
 import consecration_directive from "./consecration_directive.json";
 import control_inoculum from "./control_inoculum.json";
 import conviction_countermeasure from "./conviction_countermeasure.json";
 import cryo_lock from "./cryo_lock.json";
+import cyclonic_vitalizer from "./cyclonic_vitalizer.json";
 import devotion_paradigm from "./devotion_paradigm.json";
 import discordian_agitator from "./discordian_agitator.json";
 import displacement_index from "./displacement_index.json";
+import divination_algorithm from "./divination_algorithm.json";
+import dominion_imperative from "./dominion_imperative.json";
+import encrypted_command from "./encrypted_command.json";
+import eschatonic_well from "./eschatonic_well.json";
+import excogitation_conspectus from "./excogitation_conspectus.json";
 import force_barrier from "./force_barrier.json";
+import gremian_tormentor from "./gremian_tormentor.json";
+import hallowed_desecration from "./hallowed_desecration.json";
 import havoc_spiral from "./havoc_spiral.json";
 import instability_equation from "./instability_equation.json";
 import malediction_rubric from "./malediction_rubric.json";
@@ -18,10 +29,17 @@ import nihilation_vortex from "./nihilation_vortex.json";
 import null_collider from "./null_collider.json";
 import parabellum_chrysalis from "./parabellum_chrysalis.json";
 import paralytic_disruptor from "./paralytic_disruptor.json";
+import penumbral_obscuration from "./penumbral_obscuration.json";
+import photochimeric_mantle from "./photochimeric_mantle.json";
 import plexus_densifier from "./plexus_densifier.json";
+import psychodynamic_infuser from "./psychodynamic_infuser.json";
 import psychonomic_lash from "./psychonomic_lash.json";
 import pyrokinetic_surge from "./pyrokinetic_surge.json";
+import recall_initiative from "./recall_initiative.json";
+import reclamation_principle from "./reclamation_principle.json";
+import recurrent_apotheosis from "./recurrent_apotheosis.json";
 import reiteration_complex from "./reiteration_complex.json";
+import revelation_matrix from "./revelation_matrix.json";
 import tachytelic_metamorphosis from "./tachytelic_metamorphosis.json";
 import temporal_cycle from "./temporal_cycle.json";
 import umbral_transvection from "./umbral_transvection.json";
@@ -30,17 +48,28 @@ import volition_anchor from "./volition_anchor.json";
 
 const cyphersData = {
 	acheronian_venediction: acheronian_venediction,
+	aggression_theorem: aggression_theorem,
+	arcanesscent_regenerator: arcanesscent_regenerator,
 	arcane_synthesis: arcane_synthesis,
 	atrophic_decomposer: atrophic_decomposer,
 	cacophonic_declination: cacophonic_declination,
+	canker_bane: canker_bane,
 	consecration_directive: consecration_directive,
 	control_inoculum: control_inoculum,
 	conviction_countermeasure: conviction_countermeasure,
 	cryo_lock: cryo_lock,
+	cyclonic_vitalizer: cyclonic_vitalizer,
 	devotion_paradigm: devotion_paradigm,
 	discordian_agitator: discordian_agitator,
 	displacement_index: displacement_index,
+	divination_algorithm: divination_algorithm,
+	dominion_imperative: dominion_imperative,
+	encrypted_command: encrypted_command,
+	eschatonic_well: eschatonic_well,
+	excogitation_conspectus: excogitation_conspectus,
 	force_barrier: force_barrier,
+	gremian_tormentor: gremian_tormentor,
+	hallowed_desecration: hallowed_desecration,
 	havoc_spiral: havoc_spiral,
 	instability_equation: instability_equation,
 	malediction_rubric: malediction_rubric,
@@ -49,10 +78,17 @@ const cyphersData = {
 	null_collider: null_collider,
 	parabellum_chrysalis: parabellum_chrysalis,
 	paralytic_disruptor: paralytic_disruptor,
+	penumbral_obscuration: penumbral_obscuration,
+	photochimeric_mantle: photochimeric_mantle,
 	plexus_densifier: plexus_densifier,
+	psychodynamic_infuser: psychodynamic_infuser,
 	psychonomic_lash: psychonomic_lash,
 	pyrokinetic_surge: pyrokinetic_surge,
+	recall_initiative: recall_initiative,
+	reclamation_principle: reclamation_principle,
+	recurrent_apotheosis: recurrent_apotheosis,
 	reiteration_complex: reiteration_complex,
+	revelation_matrix: revelation_matrix,
 	tachytelic_metamorphosis: tachytelic_metamorphosis,
 	temporal_cycle: temporal_cycle,
 	umbral_transvection: umbral_transvection,
