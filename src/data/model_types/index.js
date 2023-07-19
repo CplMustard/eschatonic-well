dfed96a67434a70b3453fe50c247ccb7a2663b80
Jack Anderson <corporalmustard@gmail.com>
2023-07-18 23:45:55 -0700

    add model type definitions

---
+++ src/data/model_types/index.js
@@ -0,0 +1,20 @@
+import aeon from "./aeon.json";
+import attachment from "./attachment.json";
+import champion from "./champion.json";
+import hero from "./hero.json";
+import solo from "./solo.json";
+import squad from "./squad.json";
+import vehicle from "./vehicle.json";
+import warjack from "./warjack.json";
+
+const model_typesData = {
+	aeon: aeon,
+	attachment: attachment,
+	champion: champion,
+	hero: hero,
+	solo: solo,
+	squad: squad,
+	vehicle: vehicle,
+	warjack: warjack
+}
+export default model_typesData;
