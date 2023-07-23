574cfe324ee8b040122a2953882c908c003e2e00
Jack Anderson <corporalmustard@gmail.com>
2023-07-23 00:52:00 -0700

    add hidden model types

--- src/data/model_types/index.js
+++ src/data/model_types/index.js
@@ -2,9 +2,11 @@ import aeon from "./aeon.json";
 import attachment from "./attachment.json";
 import champion from "./champion.json";
 import hero from "./hero.json";
+import mantlet from "./mantlet.json";
 import solo from "./solo.json";
 import squad from "./squad.json";
 import vehicle from "./vehicle.json";
+import void_gate from "./void_gate.json";
 import warjack from "./warjack.json";
 
 const model_typesData = {
@@ -12,9 +14,11 @@ const model_typesData = {
 	attachment: attachment,
 	champion: champion,
 	hero: hero,
+	mantlet: mantlet,
 	solo: solo,
 	squad: squad,
 	vehicle: vehicle,
+	void_gate: void_gate,
 	warjack: warjack
 }
 export default model_typesData;
