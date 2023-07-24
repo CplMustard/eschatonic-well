af18fe206cb65446dd178b335a53a712d9615714
Jack Anderson <corporalmustard@gmail.com>
2023-07-24 13:55:34 -0700

    hide models with hidden subtypes such as champion

--- src/CardList.js
+++ src/CardList.js
@@ -12,9 +12,12 @@ function CardList(props) {
     Object.entries(cardGroups).sort().forEach(([key, value]) => {
         if (!hideHiddenTypes || (modelTypesData[key] && !modelTypesData[key].hidden)) {
             const cardComponents = []
-            value.forEach((card, index) =>
-                cardComponents.push(<li key={index}><button onClick={() => handleCardClicked(card.id)}>{card.name}</button>{cardActionClicked && <button onClick={() => cardActionClicked(card.id)}>{cardActionText}</button>}</li>)
-            )
+            value.forEach((card, index) => {
+                const hasHiddenSubtype = hideHiddenTypes && card.subtypes ? card.subtypes.every((subtype) => modelTypesData[subtype].hidden) : false;
+                if(!hasHiddenSubtype) {
+                    cardComponents.push(<li key={index}><button onClick={() => handleCardClicked(card.id)}>{card.name}</button>{cardActionClicked && <button onClick={() => cardActionClicked(card.id)}>{cardActionText}</button>}</li>);
+                }
+            })
             cardGroupComponents.push(<div key={key}><h4>{key}</h4><li><ul>{cardComponents}</ul></li></div>)
         }
     })
