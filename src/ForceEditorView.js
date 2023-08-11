3948970390eea0099f0dd354d27f83929c1053f7
Jack Anderson <corporalmustard@gmail.com>
2023-08-11 01:09:34 -0700

    save/load functionality

--- src/ForceEditorView.js
+++ src/ForceEditorView.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
+import sanitize from "sanitize-filename";
 
 import { copyForceToText } from "./util/copyForceToText";
 import { useLocalStorage } from "./util/useLocalStorage";
@@ -8,6 +9,9 @@ import ForceEditor from './ForceEditor';
 
 import { factionsData, forceSizesData } from './data'
 
+const forcesPath = "eschatonic-well/forces/"
+const forcesExtension = ".esch"
+
 function ForceEditorView() {
     const [factionId, setFactionId] = useLocalStorage("factionId", "all");
     const [forceSize, setForceSize] = useLocalStorage("forceSize", {});
@@ -15,6 +19,23 @@ function ForceEditorView() {
     const [forceName, setForceName] = useLocalStorage("forceName", "New Force");
     const [forceModelsData, setForceModelsData] = useLocalStorage("forceModelsData", []);
     const [forceCyphersData, setForceCyphersData] = useLocalStorage("forceCyphersData", []);
+
+    const [forcesDirty, setForcesDirty] = useState(true);
+    const [loadForceButtons, setLoadForceButtons] = useState([]);
+
+
+    useEffect(() => {
+        listForces().then((result) => {
+            if(forcesDirty) {
+                const newLoadForceButtons = [];
+                result.files.forEach((file, index) => {
+                    newLoadForceButtons.push(<div key={index}><button onClick={() => loadForce(file.name)}>{file.name}</button><button onClick={() => deleteForce(file.name)}>DELETE</button></div>);
+                    setLoadForceButtons(newLoadForceButtons);
+                });
+                setForcesDirty(false);
+            }
+        });
+    }, [forcesDirty])
     
     function changeFaction(id) {
         setFactionId(id);
@@ -30,12 +51,22 @@ function ForceEditorView() {
         setForceCyphersData([]);
     }
 
+    const listForces = async () => {
+        const result = await Filesystem.readdir({
+            path: forcesPath,
+            directory: Directory.Data
+        });
+        
+        console.log(result);
+        return result;
+    }
+
     const saveForce = async (forceName, factionId, forceSize, forceModelsData, forceCyphersData) => {
         const json = {"forceName": forceName, "factionId": factionId, "forceSize": forceSize, "forceModelsData": forceModelsData, "forceCyphersData": forceCyphersData};
-
+        const filename = sanitize(forceName);
         try {
             const result = await Filesystem.writeFile({
-                path: `eschatonic-well/forces/${forceName}.esch`,
+                path: `${forcesPath}${filename}${forcesExtension}`,
                 data: JSON.stringify(json),
                 directory: Directory.Data,
                 encoding: Encoding.UTF8,
@@ -43,33 +74,67 @@ function ForceEditorView() {
             });
             
             console.log(result);
+            setForcesDirty(true);
             return result;
         } catch (e) {
             console.log(e);
         }
-
     }
 
-    function loadForce(fileName) {
-        const json = JSON.stringify(forceModelsData.concat(forceCyphersData));
-        console.log(forceName + ": \n");
-        console.log(json);
+    const loadForce = async (filename) => {
+        try {
+            const result = await Filesystem.readFile({
+                path: `${forcesPath}${filename}`,
+                directory: Directory.Data,
+                encoding: Encoding.UTF8,
+            });
+            
+            const json = JSON.parse(result.data);
+            console.log(json);
+            setForceName(json.forceName);
+            setFactionId(json.factionId);
+            setForceSize(json.forceSize);
+            setForceModelsData(json.forceModelsData);
+            setForceCyphersData(json.forceCyphersData);
+        } catch (e) {
+            console.log(e);
+        }
     }
 
-    const factionButtons = []
+    const deleteForce = async (filename) => {
+        try {
+            const result = await Filesystem.deleteFile({
+                path: `${forcesPath}${filename}`,
+                directory: Directory.Data,
+            });
+            console.log(result);
+            setForcesDirty(true);
+            // For some reason we need to clear out the buttons if it's the last force being deleted
+            if((await listForces()).files.length === 0) {
+                setLoadForceButtons([]);
+            }
+            return result;
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const factionButtons = [];
     Object.entries(factionsData).forEach(([key, value]) => {
         if(!value.hidden) {
             factionButtons.push(<button key={key} onClick={() => changeFaction(value.id)}>{value.name}</button>);
         }
     });
     factionButtons.push(<button key={"custom"} onClick={() => changeFaction("")}>ALL</button>);
-    const forceSizeButtons = []
+
+    const forceSizeButtons = [];
     Object.entries(forceSizesData).sort((a, b) => a[1].units-b[1].units).forEach(([key, value]) => {
         forceSizeButtons.push(<button key={key} onClick={() => changeForceSize(value)}>{`${value.name} (${value.units} / ${value.hero_solos})`}</button>);
     });
     forceSizeButtons.push(<button key={"custom"} onClick={() => changeForceSize({})}>CUSTOM</button>);
     return (
         <div className="container">
+            Forces:{loadForceButtons}<br/>
             {factionButtons}<br/>
             {forceSizeButtons}<br/>
             <br/>
