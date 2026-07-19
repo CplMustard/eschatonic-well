ff9a8a232f7d7163193310011e7ec26aabfdb0d3
Jack Anderson <corporalmustard@gmail.com>
2026-07-18 17:36:45 -0700

    style landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,8 +1,10 @@
 import React, { createRef }from "react";
 import { useHistory } from "react-router-dom";
-import { IonPage, IonContent, IonHeader, IonButton } from "@ionic/react";
+import { IonPage, IonContent, IonHeader, IonFooter, IonText, IonImg, IonButton, IonGrid, IonCol, IonRow} from "@ionic/react";
 
 import VersionNumber from "./VersionNumber";
+
+import logo from "./assets/images/eschatonic-well.png";
 
 function LandingPage() {
     const history = useHistory();
@@ -14,16 +16,41 @@ function LandingPage() {
                 <VersionNumber/>
             </IonHeader>
             <IonContent ref={contentRef}>
-                <IonButton onClick={() => history.push("/editor")}>
-                    CREATE FORCE
-                </IonButton>
-                <IonButton onClick={() => history.push("/play")}>
-                    PLAY GAME
-                </IonButton>
-                <IonButton onClick={() => history.push("/cards")}>
-                    VIEW CARDS
-                </IonButton>
+                <div className={"landing-page"}>
+                    <IonImg className={"logo"} src={logo}></IonImg>
+                    <IonText className={"title"}><h1>ESCHATONIC WELL</h1></IonText>
+                    <IonText className={"title"}><h4>The Unofficial Companion App for Warcaster: Neo Mechanika</h4></IonText>
+                    <IonGrid>
+                        <IonRow>
+                            <IonCol>
+                                <IonButton expand={"full"} onClick={() => history.push("/editor")}>
+                                    <IonText><h2>CREATE FORCE</h2></IonText>
+                                </IonButton>
+                            </IonCol>
+                        </IonRow>
+                        <IonRow>
+                            <IonCol>
+                                <IonButton expand={"full"} onClick={() => history.push("/play")}>
+                                    <IonText><h2>PLAY GAME</h2></IonText>
+                                </IonButton>
+                            </IonCol>
+                        </IonRow>
+                        <IonRow>
+                            <IonCol>
+                                <IonButton expand={"full"} onClick={() => history.push("/cards")}>
+                                    <IonText><h2>VIEW CARDS</h2></IonText>
+                                </IonButton>
+                            </IonCol>
+                        </IonRow>
+                    </IonGrid>
+                </div>
             </IonContent>
+            <IonFooter className={"footer-text"}>
+                <IonText>
+                    <p>Copyright (c) 2026 Jack Anderson</p>
+                    <p>All trademarks contained herein including Steamforged Games (c), Warcaster (r), warjack (r), Marcher Worlds, Aeternus Continuum, Iron Star Alliance, The Empyreans, Coalition Rangers, The Thousand Worlds, The Hyperuranion, Arcanessence, and their logos are property of Steamforged Games Ltd, Osprey House, 217-227 Broadway, Salford, Manchester, M50 2UE, United Kingdom</p>
+                </IonText>
+            </IonFooter>
         </IonPage>
     );
 }
